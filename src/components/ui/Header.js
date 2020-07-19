60845e9ee45c2ea2d4f4a1b9b3d65182a6dd1b6a
nigamstaimoor <ta99870@gmail.com>
2020-07-19 13:36:48 +0500

    toolbar settings

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,46 +1,64 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import { makeStyles } from "@material-ui/styles";
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/tab";
 
+import logo from "../../assets/logo.svg";
 
-const useStyles = makeStyles(theme => ({
-    toolbarMargin: {
-        ...theme.mixins.toolbar
-    },
-}))
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "2em",
+  },
+  logo: {
+    height: "6em",
+  },
+  tabsContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
+}));
 
 function ElevationScroll(props) {
-    const { children } = props;
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
 
- const Header = () => {
-
-    const classes = useStyles()
-
-    return (
-        <React.Fragment>
-            <ElevationScroll>
-                <AppBar position="fixed" color= "primary">
-                    <Toolbar>
-                        <Typography variant= "h3">
-                            ARC Development
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </ElevationScroll>
-            <div className={classes.toolbarMargin}/>
-        </React.Fragment>   
-    )
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
 }
- export default Header
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar disableGutters>
+            <img alt="Arc Development" className={classes.logo} src={logo} />
+            <Tabs className={classes.tabsContainer}>
+              <Tab className={classes.tab} label="Home"></Tab>
+              <Tab className={classes.tab} label="Service"></Tab>
+              <Tab className={classes.tab} label="The Revolution"></Tab>
+              <Tab className={classes.tab} label="Abous Us"></Tab>
+              <Tab className={classes.tab} label="Contact US"></Tab>
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
+  );
+};
+export default Header;
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,15 +1,13 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
-
-const arcBlue= "#0B72B9"
-const arcOrange= "#FFBA60"
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
 
 export default createMuiTheme({
   palette: {
-    common:{
+    common: {
       blue: `${arcBlue}`,
-      orange: `${arcOrange}`
-
+      orange: `${arcOrange}`,
     },
     primary: {
       main: `${arcBlue}`,
@@ -18,9 +16,12 @@ export default createMuiTheme({
       main: `${arcOrange}`,
     },
   },
-  typography:{
-    h3: {
-      fontWeight : 300
-    }
-  }
+  typography: {
+    tab: {
+      fontFamily: "raleway",
+      fontWeight: 700,
+      textTransform: "none",
+      fontSize: "1rem",
+    },
+  },
 });
